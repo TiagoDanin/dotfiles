426507d548731ba62e6f8bb5f09fac2969062ac9
TiagoDanin <TiagoDanin@outlook.com>
2018-04-09 10:04:30 -0300

    Build dotfiles root

---
+++ Scripts/backupRoot/build.sh
@@ -0,0 +1,8 @@
+sudo cp -r /etc/xdg/autostart/ ~/Scripts/backupRoot
+sudo cp -r /usr/share/glib-2.0/schemas/ ~/Scripts/backupRoot
+sudo cp -r /usr/share/applications/ ~/Scripts/backupRoot
+ls /usr/share/plank/themes/ > ~/Scripts/backupRoot/lists/plank-themes
+ls /usr/share/themes/ > ~/Scripts/backupRoot/lists/gnome-themes
+ls /usr/share/icons/ > ~/Scripts/backupRoot/lists/icons
+ls /usr/share/fonts/ > ~/Scripts/backupRoot/lists/fonts
+ls /usr/share/gnome-shell/extensions/ > ~/Scripts/backupRoot/lists/gnome-extensions
