798f65ff61469dd395534a4cd4b8508142974838
TiagoDanin <TiagoDanin@outlook.com>
2018-04-09 11:03:59 -0300

    Update in 04/09/18-11:03

--- Scripts/backupRoot/build.sh
+++ Scripts/backupRoot/build.sh
@@ -12,3 +12,9 @@ ls /usr/share/icons/ > ~/Scripts/backupRoot/lists/icons
 ls /usr/share/fonts/ > ~/Scripts/backupRoot/lists/fonts
 ls /usr/share/gnome-shell/extensions/ > ~/Scripts/backupRoot/lists/gnome-extensions
 
+dconf dump /org/gnome/desktop/background/ > ~/Scripts/backupRoot/schemas/background
+dconf dump /net/launchpad/plank/ > ~/Scripts/backupRoot/schemas/plank
+dconf dump /org/gnome/desktop/screensaver/ > ~/Scripts/backupRoot/schemas/screensaver
+dconf dump /org/gnome/desktop/interface/ > ~/Scripts/backupRoot/schemas/interface
+dconf dump /org/gnome/desktop/wm/preferences/ > ~/Scripts/backupRoot/schemas/preferences
+dconf dump /org/gnome/shell/ > ~/Scripts/backupRoot/schemas/gnome-shell
