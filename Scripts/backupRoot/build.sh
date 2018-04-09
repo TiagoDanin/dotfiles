238e801a4849ab9fc8b589b441450b984c17fb87
TiagoDanin <TiagoDanin@outlook.com>
2018-04-09 10:48:01 -0300

    Update in 04/09/18-10:48

--- Scripts/backupRoot/build.sh
+++ Scripts/backupRoot/build.sh
@@ -1,8 +1,14 @@
+sudo rm -r -f autostart
+sudo rm -r -f applications
+sudo rm -r -f lists
+mkdir lists
+sudo rm -r -f schemas
+mkdir schemas
 sudo cp -r /etc/xdg/autostart/ ~/Scripts/backupRoot
-sudo cp -r /usr/share/glib-2.0/schemas/ ~/Scripts/backupRoot
 sudo cp -r /usr/share/applications/ ~/Scripts/backupRoot
 ls /usr/share/plank/themes/ > ~/Scripts/backupRoot/lists/plank-themes
 ls /usr/share/themes/ > ~/Scripts/backupRoot/lists/gnome-themes
 ls /usr/share/icons/ > ~/Scripts/backupRoot/lists/icons
 ls /usr/share/fonts/ > ~/Scripts/backupRoot/lists/fonts
 ls /usr/share/gnome-shell/extensions/ > ~/Scripts/backupRoot/lists/gnome-extensions
+
